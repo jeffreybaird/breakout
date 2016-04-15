71227dfd89f66db007b10e79b47fb926494e1c9a
jeffreybaird <jeff@jeffreyleebaird.com>
2016-04-15 12:31:48 -0700

    Trying to get the paddle to move along the x-axis

--- breakout.elm
+++ breakout.elm
@@ -6,6 +6,7 @@ import Graphics.Collage exposing (..)
 import Window
 import Color exposing (rgb)
 import Text
+import Keyboard
 
 -- representation of the Game
 
@@ -15,16 +16,14 @@ type State = Play | Pause
 
 type alias Input =
   {
-    delta  : Time.Time
-  , paddle : Int
-  , space  : Bool
+    paddle : Int
+  , delta  : Time.Time
 }
 
 type alias Game =
   {
-    ball    : Ball
+    state   : State
   , player  : Player
-  , state   : State
   }
 
 type alias Object a =
@@ -39,7 +38,14 @@ type alias Ball =
   Object {}
 
 type alias Player =
-  Object {}
+  Object {score : Int}
+
+-- Model
+
+-- player -> determines the position of the paddle
+makePlayer : Float -> Player
+makePlayer x =
+  {x=x, y=0.0, vy=0.0, vx=0.0,score=0}
 
 
 -- Track the passage of Time
@@ -50,14 +56,59 @@ defaultGame : Game
 defaultGame =
     {
       state   = Pause
-    , ball    = {x=0, y=0, vy=200, vx=200}
-    , player = {vx = 0.0, vy= 0.0, x= 0.0, y = 0.0}
+    , player = makePlayer(halfWidth - 10)
   }
+
+input : Signal Input
+input =
+  Signal.sampleOn delta <|
+    Signal.map2 Input
+      (Signal.map .x Keyboard.arrows)
+      delta
+
+-- Update
+
+stepObj : Time.Time -> Object a -> Object a
+stepObj t ({x,y,vx,vy} as obj) =
+    { obj |
+        x = x + vx * t,
+        y = y + vy * t
+    }
+
+stepPlayer : Time.Time -> Player -> Player
+stepPlayer(time, player) =
+    let player' = stepObj time { player | vx = toFloat 300 }
+        x'      = clamp (22-halfHeight) (halfHeight-22) player'.x
+        score'  = player.score + 1
+
+    in
+      { player' | x = x', score = score' }
+
+updateGame : Game -> Player -> Game
+updateGame game player_lcl =
+  {game | player = player_lcl}
+
+stepGame : Input -> Game -> Game
+stepGame input game =
+  let
+    {paddle,delta} = input
+    {state,player} = game
+    player' = stepPlayer delta player
+  in
+    {game | player = player'}
+
+gameState : Signal Game
+gameState =
+    Signal.foldp stepGame defaultGame input
 
 -- View
 
 (gameWidth,gameHeight) = (600,400)
 (halfWidth,halfHeight) = (300,200)
+
+displayObj : Object a -> Shape -> Form
+displayObj obj shape =
+    move (obj.x, obj.y) (filled Color.white shape)
 
 breakoutCharcoal : Color.Color
 breakoutCharcoal = rgb 54 69 79
@@ -69,8 +120,8 @@ txt : (Text.Text -> Text.Text) -> String -> Graphics.Element.Element
 txt f = leftAligned << f << Text.monospace << Text.color textIvory << Text.fromString
 
 
-display : (Int, Int) -> Graphics.Element.Element
-display (w,h) =
+display : (Int, Int) -> Game -> Graphics.Element.Element
+display (w,h) {state,player} =
     let scores : Graphics.Element.Element
         scores =
             toString 10
@@ -78,9 +129,11 @@ display (w,h) =
     in
       container w h middle <|
       collage gameWidth gameHeight
-       [ filled breakoutCharcoal   (rect gameWidth gameHeight)]
+       [ filled breakoutCharcoal   (rect gameWidth gameHeight)
+       , displayObj player (rect 10 40)
+       ]
 
 
 main : Signal Graphics.Element.Element
 main =
-    Signal.map display Window.dimensions
+    Signal.map2 display Window.dimensions gameState
